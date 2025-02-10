e779311691f09b1ef4f68da007523ef540593869
tobiasengberg <tobias.engberg@exilic.se>
2025-02-10 10:20:33 +0100

    Edit mousemove

--- line-drawing.js
+++ line-drawing.js
@@ -1,6 +1,4 @@
 
-let xOrigin;
-let yOrigin;
 let createPath;
 let line = {x1: 0, y1: 0, x2: 0, y2: 0};
 let state = 0;
@@ -10,43 +8,29 @@ let createSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 createSVG.setAttribute("width", window.innerWidth);
 createSVG.setAttribute("height", window.innerHeight);
 document.getElementsByTagName("body")[0].appendChild(createSVG);
-console.log(window);
-
-
-
 
 
 window.addEventListener("mousedown", function(e){
     if (state == 0) {
-        
-        xOrigin = e.pageX;
-        yOrigin = e.pageY;
-        line.x1 = xOrigin;
-        line.y1 = yOrigin;
+        createPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        createSVG.appendChild(createPath);
+        line.x1 = e.pageX;
+        line.y1 = e.pageY;
         state = 1;
     } 
 })
 
-window.addEventListener("mouseover", function(e){
+window.addEventListener("mousemove", function(e){
     if (state == 1){
-        createPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        createPath.setAttribute("d", "M " + line.x1 + " " + line.y1 + " L " + e.pageX + " " + e.pageY);
         line.x2 = e.pageX;
         line.y2 = e.pageY;
-        createPath.setAttribute("d", "M " + xOrigin + " " + yOrigin + " L " + e.pageX + " " + e.pageY);
-        createSVG.appendChild(createPath);
-        state = 2;
-    } else if (state == 2){
-        line.x2 = e.pageX;
-        line.y2 = e.pageY;
-        createPath.setAttribute("d", "M " + xOrigin + " " + yOrigin + " L " + e.pageX + " " + e.pageY);
     }
 })
 
-window.addEventListener("mouseup", function(e){
-    if (state == 2){
-        state = 0;
-        lines.push(line);
-    }
-    
+window.addEventListener("mouseup", function(e){ 
+    state = 0;
+    lines.push({...line});  
+    console.log(lines);
 })
 
