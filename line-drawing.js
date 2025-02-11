80911cb8f34bb0474536770c658b7fc3135a93e2
tobiasengberg <tobias.engberg@exilic.se>
2025-02-11 10:35:30 +0100

    Add API calls for login and logout

--- line-drawing.js
+++ line-drawing.js
@@ -3,6 +3,8 @@ let createPath;
 let line = {x1: 0, y1: 0, x2: 0, y2: 0};
 let state = 0;
 let lines = [];
+let isLoggedIn = false;
+const authButton = document.getElementById("AuthButton");
 
 let createSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 createSVG.setAttribute("width", window.innerWidth);
@@ -34,3 +36,56 @@ window.addEventListener("mouseup", function(e){
     console.log(lines);
 })
 
+
+function login() {
+    fetch("http://localhost:5124/session/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify ({
+            email: "greg@mail.com",
+            password: "1234",
+        })
+    })
+        .then(response => response.json())
+        .then(data => console.log(data));
+        isLoggedIn = true;
+        authButton.innerText = "Logout";
+}
+
+function logout() {
+    fetch("http://localhost:5124/session/logout", {
+        headers: {
+            "Accept": "application/json"
+    }})
+        .then(response => response.json())
+        .then(data => console.log(data));
+        isLoggedIn = false;
+        
+        authButton.innerText = "Login";
+
+}
+
+function saveGraph() {
+    fetch("http://localhost:5124/graph", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify ({
+            id: "34",
+            lines: lines
+        })
+    })
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+document.getElementById("SaveGraph").addEventListener("click", saveGraph);
+
+authButton.addEventListener("click", () => {
+    isLoggedIn ? logout() : login();
+});
